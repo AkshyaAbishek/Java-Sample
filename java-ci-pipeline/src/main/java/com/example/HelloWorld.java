8ecb76cff90b229e2aadedf0524c9a328bc7a384
unknown <akshya.a.pro@gmail.com>
2025-09-13 16:44:27 +0530

    Add folder java-ci-pipeline

---
+++ java-ci-pipeline/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example;
+public class HelloWorld {
+ public String greet(String name) {
+ return "Hello, " + name + "!";
+ }
+ public static void main(String[] args) {
+ HelloWorld hw = new HelloWorld();
+ System.out.println(hw.greet("World"));
+ }
+}
---
+++ java-ci-pipeline/src/main/java/com/example/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class HelloWorldTest {
+ @Test
+ public void testGreet() {
+ HelloWorld hw = new HelloWorld();
+ String result = hw.greet("Jenkins");
+ assertEquals("Hello, Jenkins!", result);
+ }
+}
